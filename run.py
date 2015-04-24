047444db8ad794b70710adb54f3a955591ebebe4
Jack Humbert <jack.humb@gmail.com>
2015-04-24 14:37:14 -0400

    ok

--- run.py
+++ run.py
@@ -23,6 +23,7 @@ import requests
 from StringIO import StringIO
 import copy
 import webcolors
+from HTMLParser import HTMLParser
 
 app = Flask(__name__)
 app.config.update(dict(
@@ -47,8 +48,8 @@ class Key(object):
     text = "#000000"
     labels =[]
     align = 4
-    fontheight = 3
-    fontheight2 = 3 
+    fontheight = 3.0
+    fontheight2 = 3.0
     rotation_angle = 0.0
     rotation_x = 0.0
     rotation_y = 0.0
@@ -87,16 +88,16 @@ def deserialise(rows):
                         current.rotation_y = cluster['y'] = key['ry']
                     if 'a' in key:
                         current.align = key['a']
-                    if hasattr(key, 'f'):
-                        current.fontheight = current.fontheight2 = key.f
-                    if hasattr(key, 'f2'):
-                        current.fontheight2 = key.f2
+                    if 'f' in key:
+                        current.fontheight = current.fontheight2 = float(key['f'])
+                    if 'f2' in key:
+                        current.fontheight2 = (key['f2'])
                     if hasattr(key, 'p'):
                         current.profile = key.p
                     if 'c' in key:
-                        current.color = key['c']
+                        current.color = key['c'].replace(";", "")
                     if 't' in key:
-                        current.text = key['t']
+                        current.text = key['t'].replace(";", "")
                     if 'x' in key:
                         current.x += float(key['x'])
                     if 'y' in key:
@@ -109,19 +110,20 @@ def deserialise(rows):
                         current.x2 = float(key['x2'])
                     if 'y2' in key:
                         current.y2 = float(key['y2'])
-                    if hasattr(key, 'w2'):
-                        current.width2 = key.w2
-                    if hasattr(key, 'h2'):
-                        current.height2 = key.h2
+                    if 'w2' in key:
+                        current.width2 = float(key['w2'])
+                    if 'h2' in key:
+                        current.height2 = float(key['h2'])
                     if hasattr(key, 'n'):
                         current.nub = key.n
                     if hasattr(key, 'l'):
                         current.stepped = key.l
                     if hasattr(key, 'g'):
                         current.ghost = key.g
-
             # End of the row
             current.y += 1.0;
+        elif 'backcolor' in row:
+            meta['backcolor'] = row['backcolor'].replace(";", "")
         current.x = current.rotation_x
     return { "meta":meta, "keys":keys }
 
@@ -137,7 +139,6 @@ def render_keys(kb):
     img = Image.new("RGB", (int(max_x - 3 + 2 * spacing), int(max_y - 3 + 2 * spacing)), meta['backcolor'])
     draw = ImageDraw.Draw(img)
     font_scale = 8
-    font = ImageFont.truetype("fonts/Roboto-Light.ttf", 14*font_scale)
     font_layer = Image.new("RGBA", (img.size[0]*font_scale, img.size[1]*font_scale))
     draw_font = ImageDraw.Draw(font_layer)
     for key in keys:
@@ -145,150 +146,88 @@ def render_keys(kb):
         y = key.y*56 - 1.5 + spacing
         w = key.width*56 - 3 
         h = key.height*56 - 3
-        light_color = webcolors.rgb_to_hex([color + 40 for color in webcolors.hex_to_rgb(key.color)])
+        light_color = webcolors.rgb_to_hex([color * 1.2 for color in webcolors.hex_to_rgb(key.color)])
         lightdark_color = webcolors.rgb_to_hex([color + 25 for color in webcolors.hex_to_rgb(key.color)])
         dark_color = webcolors.rgb_to_hex([color - 25 for color in webcolors.hex_to_rgb(key.color)])
         draw.rectangle([(x, y), (x+w, y+h)], fill=key.color, outline=dark_color)
         draw.rectangle([(x + 5, y + 5), (x+w-6, y+h-6)], fill=light_color, outline=dark_color)
-        draw_font.text(((x + 1.5 + 6)*font_scale, (y + 1.5 + 6)*font_scale), key.labels[0], key.text, font=font)
-        # # Some predefined sizes for our caps
-        # sizes = { "cap": 54, "padding": 2, "margin": 6, "spacing": 1 }
-        # def capsize(size):
-        #     return (size*sizes['cap']) - (2*sizes['spacing'])
-        # sizes.capsize = capsize
 
-        # # Lighten a color by the specified amount
-        # function lightenColor(color,mod) { 
-        #     var c = $color.sRGB8(color.r,color.g,color.b).Lab();
-        #     c.l = Math.min(100,c.l*mod);
-        #     return c.sRGB8();
-        # }
 
-        # $renderKey.getKeyRotationStyles = function(key) {
-        #     if(key.rotation_angle == 0) {
-        #         return "";
-        #     }
-        #     var angle = key.rotation_angle.toString() + "deg";
-        #     var origin = (sizes.capsize(key.rotation_x) + sizes.margin).toString() + "px " + (sizes.capsize(key.rotation_y) + sizes.margin).toString() + "px";
-        #     return "transform: rotate("+angle+"); -ms-transform: rotate("+angle+"); -webkit-transform: rotate("+angle+"); " + 
-        #            "transform-origin: "+origin+"; -ms-transform-origin: "+origin+"; -webkit-transform-origin: "+origin+";";     
-        # };
-        
-        # # Given a key, generate the HTML needed to render it   
-        # noRenderText = [0,2,1,3,0,4,2,3];
-        # $renderKey.html = function(key, $sanitize) {
-        #     var html = "";
-        #     var capwidth = sizes.capsize(key.width), capwidth2 = sizes.capsize(key.width2);
-        #     var capheight = sizes.capsize(key.height), capheight2 = sizes.capsize(key.height2);
-        #     var capx = sizes.capsize(key.x) + sizes.margin, capx2 = sizes.capsize(key.x+key.x2)+sizes.margin;
-        #     var capy = sizes.capsize(key.y) + sizes.margin, capy2 = sizes.capsize(key.y+key.y2)+sizes.margin;
-        #     var jShaped = (capwidth2 !== capwidth) || (capheight2 !== capheight) || (capx2 !== capx) || (capy2 !== capy);
-        #     var darkColor = key.color;
-        #     var lightColor = lightenColor($color.hex(key.color), 1.2).hex();
-        #     var innerPadding = (2*sizes.margin) + (2*sizes.padding);
-        #     var borderStyle = "keyborder", bgStyle = "keybg";
+        font = ImageFont.truetype("fonts/Roboto-Light.ttf", int((8+key.fontheight*1.5)*font_scale))
+        small_font = ImageFont.truetype("fonts/Roboto-Light.ttf", 8*font_scale)
 
-        #     key.centerx = key.align&1 ? true : false;
-        #     key.centery = key.align&2 ? true : false;
-        #     key.centerf = key.align&4 ? true : false;
+        key.centerx = key.align&1;
+        key.centery = key.align&2;
+        key.centerf = key.align&4;
+        for label in key.labels:
+            text = HTMLParser().unescape(re.sub(r'<.+>', '', label))
+            text_size = draw_font.textsize(text, font)
+            small_text_size = draw_font.textsize(text, small_font)
+            text_x = (x + 1.5 + 4)*font_scale
+            text_y = (y + 1.5 + 4)*font_scale
+            # top left
+            if key.labels.index(label) == 0:
+                if key.centerx:
+                    text_x = text_x + (56*key.width-15)/2*font_scale - text_size[0]/2
+                else:
+                    text_x += 1*font_scale
+                if key.centery:
+                    text_y = text_y + (56*key.height/2 - 10)*font_scale - text_size[1]/2
+            # bottom left
+            if key.labels.index(label) == 1:
+                if key.centery:
+                    continue
+                text_y = text_y + (56*key.height - 3 - 12 - 3)*font_scale- text_size[1]
+                if key.centerx:
+                    text_x = text_x + (56*key.width-17)/2*font_scale - text_size[0]/2
+                else:
+                    text_x += 1*font_scale
+            # top right
+            if key.labels.index(label) == 2:
+                if key.centerx:
+                    continue
+            # bottom right
+            if key.labels.index(label) == 3:
+                if key.centery:
+                    continue
+                if key.centerx:
+                    continue
+                text_y = text_y + (56*key.height - 3 - 12 - 3)*font_scale - text_size[1]
 
-        #     if(key.ghost) {
-        #         borderStyle += " ghosted";
-        #         bgStyle += " ghosted";
-        #     } 
-        #     // The border
-        #     html += "<div style='width:{0}px; height:{1}px; left:{2}px; top:{3}px; background-color:{4};' class='{5}'></div>\n"
-        #                 .format( capwidth,    capheight,    capx,       capy,      darkColor,             borderStyle );
-        #     if(jShaped) {
-        #         html += "<div style='width:{0}px; height:{1}px; left:{2}px; top:{3}px; background-color:{4};' class='{5}'></div>\n"
-        #                     .format( capwidth2,   capheight2,   capx2,      capy2,     darkColor,             borderStyle );
-        #     }
-        #     // The key edges
-        #     html += "<div style='width:{0}px; height:{1}px; left:{2}px; top:{3}px; background-color:{4};' class='{5}'></div>\n"
-        #                 .format( capwidth,    capheight,    capx+1,     capy+1,    darkColor,             bgStyle );
-        #     if(jShaped) {
-        #         html += "<div style='width:{0}px; height:{1}px; left:{2}px; top:{3}px; background-color:{4};' class='{5}'></div>\n"
-        #                     .format( capwidth2,   capheight2,   capx2+1,    capy2+1,   darkColor,             bgStyle );
-        #     }
 
-        #     if(!key.ghost) {
-        #         # The top of the cap
-        #         html += "<div class='keyborder inner' style='width:{0}px; height:{1}px; left:{2}px; top:{3}px; background-color:{4}; padding:{5}px;'></div>\n"
-        #                 .format( capwidth-innerPadding, capheight-innerPadding, capx+sizes.margin, capy+(sizes.margin/2), lightColor, sizes.padding );
-        #         if(jShaped && !key.stepped) {
-        #             html += "<div class='keyborder inner' style='width:{0}px; height:{1}px; left:{2}px; top:{3}px; background-color:{4}; padding:{5}px;'></div>\n"
-        #                     .format( capwidth2-innerPadding, capheight2-innerPadding, capx2+sizes.margin, capy2+(sizes.margin/2), lightColor, sizes.padding );
-        #         }
+            # side left
+            if key.labels.index(label) == 4:
+                if key.centerf:
+                    text_x = text_x + (56*key.width-15)/2*font_scale - small_text_size[0]/2
+                text_y = text_y + (56*key.height - 16)*font_scale
+                draw_font.text((text_x, text_y), text, key.text, font=small_font)
+                continue
+            # side right
+            if key.labels.index(label) == 5:
+                if key.centerf:
+                    continue
+                text_y = text_y + (56*key.height - 16)*font_scale
+                text_x = text_x + (56*key.width - 15)*font_scale - small_text_size[0]
+                draw_font.text((text_x, text_y), text, key.text, font=small_font)
+                continue
 
-        #         var maxWidth = capwidth-(2*sizes.margin);
-        #         var maxHeight = capheight-(2*sizes.margin);
-        #         if(jShaped && !key.stepped) {
-        #             maxWidth = Math.max(capwidth,capwidth2)-(2*sizes.margin);
-        #             maxHeight = Math.max(capheight,capheight2)-(2*sizes.margin);
-        #             html += "<div class='keyfg' style='width:{0}px; height:{1}px; left:{2}px; top:{3}px; background-color:{4}; padding:{5}px; background-size:{6}px {7}px; background-position:{8}px {9}px;'>\n"
-        #                     .format( capwidth2-innerPadding, capheight2-innerPadding, capx2+sizes.margin+1, capy2+(sizes.margin/2)+1, lightColor, sizes.padding, maxWidth, maxHeight, Math.min(capx,capx2)-capx2, Math.min(capy,capy2)-capy2 );
-        #         }
-        #         html += "</div><div class='keyfg' style='width:{0}px; height:{1}px; left:{2}px; top:{3}px; background-color:{4}; padding:{5}px; background-size:{6}px {7}px; background-position:{8}px {9}px;'>\n"
-        #                 .format( capwidth-innerPadding, capheight-innerPadding, capx+sizes.margin+1, capy+(sizes.margin/2)+1, lightColor, sizes.padding, maxWidth, maxHeight, Math.min(capx,capx2)-capx, Math.min(capy,capy2)-capy );
+            # middle left
+            if key.labels.index(label) == 6:
+                if key.centery:
+                    continue
+                if key.centerx:
+                    text_x = text_x + (56*key.width-15)/2*font_scale - text_size[0]/2
+                text_y = text_y + (56*key.height/2 - 4)*font_scale - text_size[1]
+            # middle right
+            if key.labels.index(label) == 7:
+                if key.centery:
+                    continue
+                if key.centerx:
+                    continue
+                text_y = text_y + (56*key.height/2 - 4)*font_scale - text_size[1]
+                text_x = text_x + (56*key.width-15)*font_scale - text_size[0]
 
-        #         # The key labels           
-        #         var textColor = lightenColor($color.hex(key.text), 1.2).hex();
-        #         html += "<div class='keylabels' style='width:{0}px; height:{1}px;'>".format(capwidth-innerPadding, capheight-innerPadding);
-        #         key.labels.forEach(function(label,i) {
-        #             if(label && label !== "" && !(key.align&noRenderText[i])) {
-        #                 var sanitizedLabel = $sanitize(label.replace(/<([^a-zA-Z\/]|$)/,"&lt;$1"));
-        #                 html += "<div class='keylabel keylabel{2} centerx-{5} centery-{6} centerf-{7} textsize{8}' style='color:{1};width:{3}px;height:{4}px;'><div style='width:{3}px;max-width:{3}px;height:{4}px;'>{0}</div></div>\n"
-        #                             .format(sanitizedLabel, i===4||i===5 ? key.text : textColor, i+1, capwidth-innerPadding, capheight-innerPadding, 
-        #                                     key.centerx, key.centery, key.centerf, i>0 ? key.fontheight2 : key.fontheight);
-        #             }
-        #         });
-        #         html += "</div></div>";
-        #     }
-
-        #     key.rect = { x:capx, y:capy, w:capwidth, h:capheight, x2:capx+capwidth, y2:capy+capheight };
-        #     key.rect2 = { x:capx2, y:capy2, w:capwidth2, h:capheight2, x2:capx2+capwidth2, y2:capy2+capheight2 };
-        #     key.rectMax = { x:Math.min(capx,capx2), y:Math.min(capy,capy2), x2:Math.max(capx+capwidth,capx2+capwidth2), y2:Math.max(capy+capheight,capy2+capheight2) };
-        #     key.rectMax.w = key.rectMax.x2 - key.rectMax.x;
-        #     key.rectMax.h = key.rectMax.y2 - key.rectMax.y;
-
-        #     # Rotation matrix about the origin
-        #     var origin_x = sizes.capsize(key.rotation_x)+sizes.margin;
-        #     var origin_y = sizes.capsize(key.rotation_y)+sizes.margin;
-        #     key.mat = Math.transMatrix(origin_x, origin_y).mult(Math.rotMatrix(key.rotation_angle)).mult(Math.transMatrix(-origin_x, -origin_y));
-
-        #     # Construct the *eight* corner points, transform them, and determine the transformed bbox.
-        #     key.bbox = { x:9999999, y:9999999, x2:-9999999, y2:-9999999 };
-        #     var corners = [ 
-        #         {x:key.rect.x, y:key.rect.y}, 
-        #         {x:key.rect.x, y:key.rect.y2}, 
-        #         {x:key.rect.x2, y:key.rect.y}, 
-        #         {x:key.rect.x2, y:key.rect.y2},
-        #         {x:key.rect2.x, y:key.rect2.y}, 
-        #         {x:key.rect2.x, y:key.rect2.y2}, 
-        #         {x:key.rect2.x2, y:key.rect2.y}, 
-        #         {x:key.rect2.x2, y:key.rect2.y2},
-        #     ];
-        #     for(var i = 0; i < corners.length; ++i) {
-        #         corners[i] = key.mat.transformPt(corners[i]); 
-        #         key.bbox.x = Math.min(key.bbox.x, corners[i].x);
-        #         key.bbox.y = Math.min(key.bbox.y, corners[i].y);
-        #         key.bbox.x2 = Math.max(key.bbox.x2, corners[i].x);
-        #         key.bbox.y2 = Math.max(key.bbox.y2, corners[i].y);
-        #     }
-        #     key.bbox.w = key.bbox.x2 - key.bbox.x;
-        #     key.bbox.h = key.bbox.y2 - key.bbox.y;
-
-        #     # Keep an inverse transformation matrix so that we can transform mouse
-        #     # coordinates into key-space.
-        #     key.mat = Math.transMatrix(origin_x, origin_y).mult(Math.rotMatrix(-key.rotation_angle)).mult(Math.transMatrix(-origin_x, -origin_y));
-
-        #     # Determine the location of the rotation crosshairs for the key
-        #     key.crosshairs = "none";
-        #     if(key.rotation_x || key.rotation_y || key.rotation_angle) {
-        #         key.crosshairs_x = sizes.capsize(key.rotation_x) + sizes.margin;
-        #         key.crosshairs_y = sizes.capsize(key.rotation_y) + sizes.margin;
-        #         key.crosshairs = "block";
-        #     }
+            draw_font.text((text_x, text_y), text, key.text, font=font)
 
 
     font_layer.thumbnail(img.size, Image.ANTIALIAS)
